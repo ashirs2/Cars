71869e701ce08d677cf333cfdfa6344d1ba03c65
Ali Shirsalimian <ashirsalimian12@gmail.com>
2022-05-05 19:09:51 -0500

    Database Packages

---
+++ Models/Make.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Cars.Models;
+
+namespace Cars.Models
+{
+    public class Make
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Model> Models;
+
+        public Make()
+        {
+            Models = new Collection<Model>();
+        }
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cars.Models
+{
+    public class Model
+    {
+        public Model()
+        {
+
+
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Make Make { get; set; }
+
+        public int MakeId { get; set; }
+
+        
+    }
+}
